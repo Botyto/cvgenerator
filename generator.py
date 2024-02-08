fbe6ce8e7c2435698c2037681863e21eab17503e
biser <bisserkr@gmail.com>
2024-02-08 09:33:30 +0200

    Generator works on all profiles in parallel

--- generator.py
+++ generator.py
@@ -1,14 +1,12 @@
 import argparse
 import base64
-from dataclasses import dataclass
 import importlib
-import markdown
 import os
 import selenium
 import selenium.webdriver
 from selenium.webdriver.common.print_page_options import PrintOptions
-import sys
 import time
+from typing import Dict, Tuple
 import watchdog.observers
 import watchdog.events
 
@@ -27,66 +25,67 @@ def data_to_dict(profile: model.Profile):
     return _to_dict(profile)
 
 
-@dataclass
-class Config:
-    profile_path: str
-    output_base_path: str
+class Generator:
+    _profiles: Dict[str, Tuple[model.Profile, float]]
 
-    @property
-    def template_path(self):
-        return os.path.join("templates", self.profile.template)
+    def __init__(self):
+        self._profiles = {}
 
-    @property
-    def template_max_mtime(self):
-        max = None
-        for root, _, files in os.walk(self.template_path):
-            for file in files:
-                if file.endswith(".html"):
-                    mtime = os.path.getmtime(os.path.join(root, file))
-                    if max is None or mtime > max:
-                        max = mtime
-        return max
+    def all_profiles(self):
+        return [f[:-3] for f in os.listdir("input") if f.endswith(".py") and "template" not in f]
     
-    @property
-    def profile_mtime(self):
-        path = self.profile_path + ".py"
-        if not os.path.exists(path):
-            return 0
-        return os.path.getmtime(path)
-    
-    def output_path(self, ext: str):
-        os.makedirs(os.path.dirname(self.output_base_path), exist_ok=True)
-        return self.output_base_path + ext
-    
-    def output_mtime(self, ext: str):
-        path = self.output_path(ext)
-        if not os.path.exists(path):
-            return 0
-        return os.path.getmtime(self.output_path(ext))
-    
-    def needs_update(self, output_ext: str):
-        output_mtime = self.output_mtime(output_ext)
-        return self.profile_mtime > output_mtime or self.template_max_mtime > output_mtime
-    
-    @property
-    def profile(self) -> model.Profile:
-        module_path = self.profile_path.replace(os.path.sep, ".")
-        src_module = importlib.import_module(module_path)
+    def _read_profile(self, profile: str) -> model.Profile:
+        src_module = importlib.import_module("input." + profile)
         src_module = importlib.reload(src_module)
         return src_module.PROFILE
 
-    def generate_html(self):
-        print("Generating HTML...")
-        html = template.generate(self.template_path, self.profile)
-        if "markdown" in self.template_path:
-            html = markdown.markdown(html)
-        with open(self.output_path(".html"), "wt", encoding="utf-8") as fh:
+    def get_profile(self, profile: str) -> model.Profile:
+        data, read_time = self._profiles.get(profile, (None, 0))
+        if data is None or read_time < os.path.getmtime(f"input/{profile}.py"):
+            data = self._read_profile(profile)
+            self._profiles[profile] = (data, time.time())
+        return data
+
+    def needs_update(self, profile: str|None):
+        if profile is None:
+            return any(self.needs_update(p) for p in self.all_profiles())
+        profile_data = self.get_profile(profile)
+        template_max_mtime = 0.0
+        for root, _, files in os.walk(f"templates/{profile_data.template}"):
+            for file in files:
+                if file.endswith(".html"):
+                    mtime = os.path.getmtime(os.path.join(root, file))
+                    if mtime > template_max_mtime:
+                        template_max_mtime = mtime
+        profile_mtime = os.path.getmtime(f"input/{profile}.py")
+        output_mtime = os.path.getmtime(f"output/{profile}.html")
+        return profile_mtime > output_mtime or template_max_mtime > output_mtime
+
+    def generate_html(self, profile: str|None):
+        if profile is None:
+            for profile in self.all_profiles():
+                self.generate_html(profile)
+            return
+        print(f"[{profile}] Generating HTML...")
+        profile_data = self.get_profile(profile)
+        template_path = f"templates/{profile_data.template}"
+        try:
+            html = template.generate(template_path, profile_data)
+        except Exception as e:
+            print(f"[{profile}] Failed to generate HTML:", e)
+            return
+        output_path = f"output/{profile}.html"
+        with open(output_path, "wt", encoding="utf-8") as fh:
             fh.write(html)
 
-    def generate_pdf(self):
-        print("Converting to PDF...")
+    def generate_pdf(self, profile: str|None):
+        if profile is None:
+            for profile in self.all_profiles():
+                self.generate_pdf(profile)
+            return
+        print(f"[{profile}] Converting to PDF...")
         with selenium.webdriver.Edge() as driver:
-            html_path = self.output_path(".html")
+            html_path = f"output/{profile}.html"
             driver.get("file://" + os.path.abspath(html_path))
             options = PrintOptions()
             options.orientation = "portrait"
@@ -100,44 +99,33 @@ class Config:
             options.margin_top = 1.27
             pdf = driver.print_page(options)
         pdf_bytes = base64.b64decode(pdf)
-        with open(self.output_path(".pdf"), "wb") as fh:
+        pdf_path = f"output/{profile}.pdf"
+        with open(pdf_path, "wb") as fh:
             fh.write(pdf_bytes)
 
 
 class GenerateHandler(watchdog.events.FileSystemEventHandler):
-    config: Config
+    gen: Generator
 
-    def __init__(self, config: Config):
-        self.config = config
-
-    def generate(self):
-        try:
-            self.config.generate_html()
-        except Exception as e:
-            info = sys.exc_info()[2]
-            print("Failed to generate HTML:", e)
+    def __init__(self, gen: Generator):
+        self.gen = gen
 
     def on_modified(self, event):
-        for path in [self.config.template_path, self.config.profile_path]:
-            if path in event.src_path:
-                self.generate()
-                break
+        if self.gen.needs_update(None):
+            self.gen.generate_html(None)
+
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
-    args.add_argument("--profile", type=str, default="bisser")
     args.add_argument("--continuous", action="store_true")
     args = args.parse_args()
 
-    config = Config(
-        profile_path=os.path.join("input", args.profile),
-        output_base_path=os.path.join("output", args.profile),
-    )
+    gen = Generator()
 
     if args.continuous:
-        config.generate_html()
+        gen.generate_html(None)
         observer = watchdog.observers.Observer()
-        handler = GenerateHandler(config)
+        handler = GenerateHandler(gen)
         observer.schedule(handler, ".", recursive=True)
         observer.start()
         try:
@@ -146,12 +134,9 @@ if __name__ == "__main__":
         except KeyboardInterrupt:
             observer.stop()
         observer.join()
-    elif config.needs_update(".html"):
-        config.generate_html()
-        config.generate_pdf()
-        print("Testing...")
-        import test
-        test.test(config.output_path(".pdf"))
+    elif gen.needs_update():
+        gen.generate_html()
+        gen.generate_pdf()
     else:
         print("No changes detected...")
     print("Done.")
